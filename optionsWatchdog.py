febb78e696e43211711cc61d231ccb694862fa35
Larry Steinberg <larry@larsware.com>
2019-07-27 13:14:30 -0700

    added aws lambda support

--- optionsWatchdog.py
+++ optionsWatchdog.py
@@ -5,17 +5,22 @@ import re
 from datetime import datetime
 import logging
 
-#logging.basicConfig(level=logging.WARNING)
-logging.basicConfig(level=logging.DEBUG)
+#TODO: add premium to output
+
+logging.basicConfig(level=logging.WARNING)
+#logging.basicConfig(level=logging.DEBUG)
 HEADER = "   Stock DTE CurrPrice OptsPrice Type Status %OTM"
 date_format = "%Y/%m/%d"
 today = datetime.today()
 logging.debug(today)
-with open('config.json') as json_data_file:
-    config = json.load(json_data_file)
-logging.debug("config: " + json.dumps(config, indent=4))
-aws = config["aws"]
-logging.debug("aws: " + aws)
+isAWS = False
+
+#Don't need config yet...
+#with open('config.json') as json_data_file:
+    #config = json.load(json_data_file)
+#logging.debug("config: " + json.dumps(config, indent=4))
+#aws = config["aws"]
+#logging.debug("aws: " + aws)
 
 class StockOpt:
     name = ""
@@ -32,11 +37,24 @@ class StockOpt:
         alert = ""
         if self.IOTM == "ITM":
             alert = "---"
-        return "{:3} {:4} {:3} {:>7} {:>7} {:4} {:3} {:>.0f}".format(alert, self.name, self.DTE, self.currentPrice, str(self.optsPrice), self.optType, self.IOTM, self.pctIOTM)
+        return "{:3} {:4} {:3} {:>7} {:>7} {:4} {:3} {:>.0f}%".format(alert, self.name, self.DTE, self.currentPrice, str(self.optsPrice), self.optType, self.IOTM, self.pctIOTM)
+
+def lambda_handler(event, context):
+
+    logging.debug("lambda_handler enter")
+    isAWS = True
+    import boto3
+
+    r = run()
+    return {
+        'statusCode': 200,
+        'body': json.dumps(r)
+    }
 
 def yScrape(stock):
 
     logging.debug("yScrape enter")
+    bid = ""
     url = "https://finance.yahoo.com/quote/" + stock
     response = requests.get(url)
 
@@ -47,11 +65,12 @@ def yScrape(stock):
     isBid = False
 
     for tag in soup.find_all('span'):
-        #print(tag)
+        #logging.debug(tag)
         if isBid:
             bid = str(tag)
             isBid = False
             #that's all we're looking for - now
+            logging.debug("Found bid: " + bid)
             break
 
         z = re.search(r"Bid", str(tag))
@@ -63,80 +82,97 @@ def yScrape(stock):
 
 def loadOptionsData():
 
-    logging.debug("loadOptionsData enter")
-    filename = "optionsData.txt"
-    try:
-        with open(filename) as json_file:
-            data = json.load(json_file)
-            #print(json.dumps(data,indent=4))
-    except FileNotFoundError:
-        print("Error: file not found: " + filename)
-        logging.critical("Error: file not found: " + filename)
-    return data
+    if isAWS:
+        s3 = boto3.client('s3')
+        try:
+            data = s3.get_object(Bucket='larsbucket1', Key='optionsData.txt')
+            json_data = data['Body'].read()
+            return json_data
+        except Exception as e:
+            print(e)
+            raise e
+
+    else:
+        logging.debug("loadOptionsData enter")
+        filename = "optionsData.txt"
+        try:
+            with open(filename) as json_file:
+                data = json.load(json_file)
+                #print(json.dumps(data,indent=4))
+        except FileNotFoundError:
+            print("Error: file not found: " + filename)
+            logging.critical("Error: file not found: " + filename)
+        return data
 
 def parseBid(b):
-    a = b.split('>')
-    b = a[1].split(' ')
-    return float(b[0].replace(",", ""))
+    try:
+        a = b.split('>')
+        b = a[1].split(' ')
+        return float(b[0].replace(",", ""))
+    except ValueError:
+        logging.warning("Could not convert bid: " + str(b))
+        return 9999
 
+#TODO - encapsulate and call
 
-#read file into list
-data = loadOptionsData()
-logging.debug(json.dumps(data,indent=4))
+def run():
+    #read file into list
+    data = loadOptionsData()
+    logging.debug(json.dumps(data,indent=4))
 
-stockOptionsList = []
+    stockOptionsList = []
 
-for d in data["stock"]:
-    stock = d["name"]
-    optionsType = d["type"]
-    optionsPrice = float(d["price"])
-    expDate = d["date"]
+    for d in data["stock"]:
+        stock = d["name"]
+        optionsType = d["type"]
+        optionsPrice = float(d["price"])
+        expDate = d["date"]
 
-    r = yScrape(stock)
-    bid = parseBid(r)
-    #print("bid as float: " + bid)
-    #print("stock / bid: " + stock + " / " + str(bid))
+        r = yScrape(stock)
+        bid = parseBid(r)
+        #print("bid as float: " + bid)
+        #print("stock / bid: " + stock + " / " + str(bid))
 
-    so = StockOpt()
-    so.name = stock
-    so.optType = optionsType
-    so.currentPrice = bid
-    so.optsPrice = optionsPrice
-    so.expirationDate = expDate
-    a = datetime.strptime(expDate, date_format)
-    so.DTE = (a - today).days
+        so = StockOpt()
+        so.name = stock
+        so.optType = optionsType
+        so.currentPrice = bid
+        so.optsPrice = optionsPrice
+        so.expirationDate = expDate
+        a = datetime.strptime(expDate, date_format)
+        so.DTE = (a - today).days
 
-    if optionsType == "put":
-        if optionsPrice < bid:
-            #print("OTM");
-            so.IOTM = "OTM"
-            so.pctIOTM = (1 - optionsPrice/bid) * 100
+        if optionsType == "put":
+            if optionsPrice < bid:
+                #print("OTM");
+                so.IOTM = "OTM"
+                so.pctIOTM = (1 - optionsPrice/bid) * 100
+            else:
+                so.IOTM = "ITM"
+                #print("option is in the money")
         else:
-            so.IOTM = "ITM"
-            #print("option is in the money")
-    else:
-        #calls
-        if optionsPrice > bid:
-            #OTM
-            so.IOTM = "OTM"
-            so.pctIOTM = (optionsPrice/bid - 1) * 100
-            #print("OTM%:  " + str(OTMpct))
-        else:
-            so.IOTM = "ITM"
-            #print("in the money")
-    stockOptionsList.append(so)
+            #calls
+            if optionsPrice > bid:
+                #OTM
+                so.IOTM = "OTM"
+                so.pctIOTM = (optionsPrice/bid - 1) * 100
+            else:
+                so.IOTM = "ITM"
+                so.pctIOTM = (bid/optionsPrice - 1) * 100
+        stockOptionsList.append(so)
 
-#enumerate list
+    #enumerate list
 
-#report stock, price, options, in/OTM, %OTM, DTE - sort by ITM, DTE
-print(HEADER)
-#sort by ITM then DTE
-stockOptionsList.sort(key=lambda stockOptions: stockOptions.DTE)
-stockOptionsList.sort(key=lambda stockOptions: stockOptions.IOTM)
-#logging.debug("sorted: " + soSorted)
-for e in stockOptionsList:
-    print(e.toString())
+    #report stock, price, options, in/OTM, %OTM, DTE - sort by ITM, DTE
+    #TODO print based on output type and runtime
+    print(HEADER)
+    #sort by ITM then DTE
+    stockOptionsList.sort(key=lambda stockOptions: stockOptions.DTE)
+    stockOptionsList.sort(key=lambda stockOptions: stockOptions.IOTM)
+    #logging.debug("sorted: " + soSorted)
+    for e in stockOptionsList:
+        print(e.toString())
 
-exit(0)
-
+if __name__ == '__main__':
+    run()
 
